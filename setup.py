48de5b65c23e7f252e3f3f071108db5328246f8a
kvs8 <kostya.shefer.999@gmail.com>
2023-09-25 14:20:51 +0700

    bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def find_required():
 
 setup(
     name="vedro-replay",
-    version="0.1.3",
+    version="0.2.0",
     description="vedro-replay package",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
