615c64a9c46021bf1ccb97cf2cddbfb86fe07634
kvs8 <kostya.shefer.999@gmail.com>
2023-11-02 20:04:40 +0700

    Extending version support

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Konstantin Shefer",
     author_email="kostya.shefer.999@gmail.com",
-    python_requires=">=3.9",
+    python_requires=">=3.8",
     url="https://github.com/kvs8/vedro-replay",
     license="Apache-2.0",
     packages=['vedro_replay'],
@@ -24,6 +24,7 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     entry_points={
         "console_scripts": [
